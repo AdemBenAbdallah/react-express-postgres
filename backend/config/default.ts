e44e411a18a05659cc7ad706558f09f3c389d6c7
AdmeBenAbdallah <adembenabdallah.contact@gmail.com>
2024-08-19 15:20:29 +0100

    full stack app react express postgres redis

---
+++ backend/config/default.ts
@@ -0,0 +1,6 @@
+export default {
+  redisCacheExpiresIn: 60,
+  refreshTokenExpiresIn: 60,
+  accessTokenExpiresIn: 1,
+  origin: "http://localhost:5173",
+};
---
+++ backend/src/services/user.service.ts
@@ -0,0 +1,67 @@
+import { Prisma, PrismaClient, User } from "@prisma/client";
+import config from "config";
+import { omit } from "lodash";
+import redisClient from "../utils/connectRedis";
+import { signJwt } from "../utils/jwt";
+
+export const excludedFields = [
+  "password",
+  "verified",
+  "verificationCode",
+  "passwordResetAt",
+  "passwordResetToken",
+];
+
+const prisma = new PrismaClient();
+
+export const createUser = async (input: Prisma.UserCreateInput) => {
+  return (await prisma.user.create({
+    data: input,
+  })) as User;
+};
+
+export const findUser = async (
+  where: Partial<Prisma.UserWhereInput>,
+  select?: Prisma.UserSelect
+) => {
+  return (await prisma.user.findFirst({
+    where,
+    select,
+  })) as User;
+};
+
+export const findUniqueUser = async (
+  where: Prisma.UserWhereUniqueInput,
+  select?: Prisma.UserSelect
+) => {
+  return (await prisma.user.findUnique({
+    where,
+    select,
+  })) as User;
+};
+
+export const updateUser = async (
+  where: Prisma.UserWhereUniqueInput,
+  data: Prisma.UserUpdateInput,
+  select?: Prisma.UserSelect
+) => {
+  return (await prisma.user.update({ where, data, select })) as User;
+};
+
+export const signTokens = async (user: Prisma.UserCreateInput) => {
+  // 1. Create Session
+  redisClient.set(`${user.id}`, JSON.stringify(omit(user, excludedFields)), {
+    EX: config.get<number>("redisCacheExpiresIn") * 60,
+  });
+
+  // 2. Create Access and Refresh tokens
+  const access_token = signJwt({ sub: user.id }, "accessTokenPrivateKey", {
+    expiresIn: `${config.get<number>("accessTokenExpiresIn")}m`,
+  });
+
+  const refresh_token = signJwt({ sub: user.id }, "refreshTokenPrivateKey", {
+    expiresIn: `${config.get<number>("refreshTokenExpiresIn")}m`,
+  });
+
+  return { access_token, refresh_token };
+};
